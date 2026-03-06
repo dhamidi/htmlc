1be4f718852c90578c5c427378c804ce0d48f84b
Builder Agent <builder@factory.local>
2026-03-06 22:19:28 +0000

    style(blog): add neo-brutalist list styles to PostList.vue

--- examples/blog/templates/PostList.vue
+++ examples/blog/templates/PostList.vue
@@ -8,3 +8,29 @@
     </ul>
   </div>
 </template>
+
+<style>
+.post-list ul {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+}
+
+.post-list li {
+  border-bottom: 2px solid #000;
+}
+
+.post-list li:last-child {
+  border-bottom: none;
+}
+
+.post-list p {
+  font-size: 1.2rem;
+  font-weight: 700;
+  text-transform: uppercase;
+  letter-spacing: 0.05em;
+  padding: 2rem 0;
+  border: 3px solid #000;
+  text-align: center;
+}
+</style>
